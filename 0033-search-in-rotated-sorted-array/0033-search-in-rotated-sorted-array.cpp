dd0484e06e9c96ff1d6b743459217c1fccdf7e98
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2023-08-09 12:59:00 +0530

    Time: 3 ms (76.36%), Space: 11 MB (45.43%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,57 +1,28 @@
 class Solution {
-    int findPivot(vector<int>& nums,int n) {
-        int s=0,e=n-1;
-        
-        int mid=(s+e)/2;
-        
-
-        
-        while(s<e){
-           if(nums[mid]>=nums[0])
-               s=mid+1;
-            else{
-                e=mid;
-            }
-            
-            mid=(s+e)/2;
-        }
-        return s;
-    }
-    
-    
-      int search(vector<int>& nums,int s,int e, int target) {
-       
-int mid;
-    
-    while(s<=e){
-        mid=(s+e)/2;
-        if(nums[mid]==target)
-            return mid;
-        else if(nums[mid]>target){
-            e=mid-1;
-        }
-        else{
-            s=mid+1;
-        }
-        // cout<<mid<<endl;
-    }
-       return -1;
-        //   return mid;
-    }
-    
 public:
-     
-    
-    
-    int search(vector<int>& nums, int target) {
-     int pivot=findPivot(nums,nums.size());
-        cout<<pivot<<endl;
-        if(target>=nums[pivot] && target<=nums[nums.size()-1]){
-            return search(nums,pivot,nums.size()-1,target);
-        }
-        
-        else{
-            return search(nums,0,pivot-1,target);
+    int search(vector<int>& arr, int target) {
+        int s=0,e=arr.size()-1;
+        int mid;
+        while(s<=e){
+            mid=(s+e)/2;
+            if(arr[mid]==target)return mid;
+            
+            if(arr[s]<=arr[mid]){
+                if(arr[s]<=target && target<=arr[mid]){
+                    e=mid-1;
+                }
+                else {
+                    s=mid+1;
+                }
+            }
+            else {
+                if(target>=arr[mid] && target<=arr[e]){
+                    s=mid+1;
+                }
+                else {
+                    e=mid-1;
+                }
+            }
         }
         return -1;
     }
