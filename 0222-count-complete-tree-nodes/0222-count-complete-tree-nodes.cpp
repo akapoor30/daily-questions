6a77cef6195ae9029c97457e7c6f13e9395f7ad5
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2023-11-26 22:44:05 +0530

    Time: 23 ms (73.56%), Space: 31.1 MB (49.57%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,9 +11,37 @@
  */
 class Solution {
 public:
+    
+    
+    int countleft(TreeNode* root){
+        int ans=0;
+        while(root){
+            ans++;
+            root=root->left;
+        }
+        return ans;
+    }
+    
+     int countright(TreeNode* root){
+        int ans=0;
+        while(root){
+            ans++;
+            root=root->right;
+        }
+        return ans;
+    }
+    
   int countNodes(TreeNode* root) 
     {    
         if(root==NULL) return 0;
+      
+        int lh=countleft(root);
+      int rh=countright(root);
+      
+      if(lh==rh)return (1<<lh)-1;
+      
+      
+      
         return 1+countNodes(root->left)+countNodes(root->right);
     }
 };
