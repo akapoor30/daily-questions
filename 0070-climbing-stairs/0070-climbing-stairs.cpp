5dc9472df1748194e1002dbe22fdcfb11db6bcf0
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2024-01-19 12:27:48 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (8.63%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -11,7 +11,11 @@ public:
     }
     
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return f(n,dp);
+        vector<int> dp(n+1,0);
+        dp[1]=1;dp[0]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
