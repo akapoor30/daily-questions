4f554776b85127e879ad040ecc4a64006ca9d4db
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2024-02-19 14:00:52 +0530

    Time: 6835 ms (12.01%), Space: 0B (100.00%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
-select product.product_name,sales.year,sales.price from sales left join product on sales.product_id=product.product_id;
+/* Write your T-SQL query statement below */
+select p.product_name,s.year,s.price from sales s left join product p on s.product_id=p.product_id;
