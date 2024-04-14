9d2d68e7be9068c34ff116d8824e2ac3bc5daa92
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2024-04-14 12:41:15 +0530

    Time: 4 ms (64.01%), Space: 16 MB (86.30%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,15 +1,63 @@
 class Solution {
+    
+    
+    //proper binary search
+    
+    int firstOCC(vector<int>& nums, int target){
+        int low=0,high=nums.size()-1;
+        int first=-1;
+        
+        
+        while(low<=high){
+            int mid=(low+high)/2;
+            
+            if(nums[mid]==target){
+                first=mid;
+                high=mid-1;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else {
+                low=mid+1;
+            }
+        }
+        return first;
+    }
+    
+    
+        int lastOCC(vector<int>& nums, int target){
+        int low=0,high=nums.size()-1;
+        int last=-1;
+        
+        
+        while(low<=high){
+            int mid=(low+high)/2;
+            
+            if(nums[mid]==target){
+                last=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else {
+                low=mid+1;
+            }
+        }
+        return last;
+    }
+    
+    
 public:
     vector<int> searchRange(vector<int>& arr, int target) {
-        int lb=lower_bound(arr.begin(),arr.end(),target) - arr.begin();
+        int lb=firstOCC(arr,target);
+        if(lb==-1)return {-1,-1};
         
-        if(lb==arr.size() || arr[lb]!=target){
-            return {-1,-1};
-        }
+
+        int ub=lastOCC(arr,target);
         
-        int ub=upper_bound(arr.begin(),arr.end(),target) - arr.begin();
-        
-        return {lb,ub-1};
+        return {lb,ub};
         
     }
 };
