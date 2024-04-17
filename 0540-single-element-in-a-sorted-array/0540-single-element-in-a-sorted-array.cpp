04b44c0a69f467c3f3b4443f7732e3b94f44f298
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2024-04-17 16:43:29 +0530

    Time: 21 ms (40.30%), Space: 24.6 MB (85.71%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -13,10 +13,12 @@ public:
             
             if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])return nums[mid];
             
-            
+            //left side
             if((mid%2==1 && nums[mid]==nums[mid-1]) ||(mid%2==0 && nums[mid]==nums[mid+1])){
                 low=mid+1;
             }
+            
+            //right side
             else{
                 high=mid-1;
             }
