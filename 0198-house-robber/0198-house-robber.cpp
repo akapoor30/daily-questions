a80b44545a487d566a51d1a282931aad86761645
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2024-08-26 14:50:53 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (48.94%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,38 +1,26 @@
-//SAME QUESTION AS ADJCENT SUM
 class Solution {
-public:
     
-    int f(int ind,vector<int>& nums,vector<int> &dp){
-        if(ind==0)return nums[0];
-        if(ind<0)return 0;
-        
+    int f(vector<int> &nums,int ind,vector<int> &dp){
+        if(ind==0){
+            return nums[ind];
+        }
+        if(ind<0){
+            return 0;
+        }
         if(dp[ind]!=-1)return dp[ind];
         
+          int pick=nums[ind]+f(nums,ind-2,dp);
         
-        int pk=nums[ind]+f(ind-2,nums,dp);
-        int np=0+f(ind-1,nums,dp);
+        int notPick=0+f(nums,ind-1,dp);
         
-        return dp[ind]=max(pk,np);
+        return dp[ind]=max(pick,notPick);
     }
     
-    
+public:
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> dp(n,0);
-        int prev2=0;
-        int prev=nums[0];
         
-        for(int ind=1;ind<n;ind++){
-            int pk=nums[ind];
-            if(ind>1)pk+=prev2;
-            int np=0+prev;
-           
-            int curr=max(pk,np);
-            
-            prev2=prev;
-            prev=curr;
-            
-        }
-        return prev;
+        vector<int> dp(nums.size(),-1);
+        return f(nums,nums.size()-1,dp);
+        
     }
 };
