d5a5607971f0264c8cc94bcde7459bf338a0cb3b
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2024-08-26 19:10:51 +0530

    Time: 2 ms (42.10%), Space: 9.8 MB (28.78%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,41 +1,39 @@
 class Solution {
-    
-int f1(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> dp(n,0);
-        int prev2=0;
-        int prev=nums[0];
-        
-        for(int ind=1;ind<n;ind++){
-            int pk=nums[ind];
-            if(ind>1)pk+=prev2;
-            int np=0+prev;
-           
-            int curr=max(pk,np);
-            
-            prev2=prev;
-            prev=curr;
-            
-        }
-        return prev;
-    }
 public:
+    
+     int f(vector<int> &nums,int ind, vector<int> &dp){
+        if(ind==0){
+            return nums[ind];
+        }
+        if(ind<0){
+            return 0;
+        }
+         
+        if(dp[ind]!=-1)return dp[ind];
+        
+          int pick=nums[ind]+f(nums,ind-2,dp);
+        
+        int notPick=0+f(nums,ind-1,dp);
+        
+        return dp[ind]=max(pick,notPick);
+    }
+    
     int rob(vector<int>& nums) {
-        vector<int> temp1,temp2;
         
-        int n=nums.size();
+        vector<int> vec1,vec2;
         
-        if(n==1)return nums[0];
-        for(int i=0;i<n;i++){
-            if(i!=0){
-                temp1.push_back(nums[i]);
-            }
-            if(i!=n-1){
-                temp2.push_back(nums[i]);
-            }
-            
+        vector<int> dp1(nums.size()-1,-1),dp2(nums.size()-1,-1);
+        
+        vec1.push_back(nums[0]);
+        if(nums.size()==1)return nums[0];
+        for(int i=1;i<nums.size()-1;i++){
+          
+            vec1.push_back(nums[i]);
+            vec2.push_back(nums[i]);
         }
         
-        return max(f1(temp1),f1(temp2));
+        vec2.push_back(nums[nums.size()-1]);
+        
+        return max(f(vec1,nums.size()-2,dp1),f(vec2,nums.size()-2,dp2));
     }
 };
