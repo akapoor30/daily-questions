1b0756b6a467fd43ca3ce04e4799ac6f6ff2e5ab
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2024-08-27 23:37:40 +0530

    Time: 8 ms (37.08%), Space: 15.4 MB (5.14%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -24,32 +24,34 @@ public:
         int n=arr[0].size();
         
         vector<vector<int>> dp(m,vector<int>(n,0));
-        
-        dp[0][0]=arr[0][0];
+        vector<int> prev(n,0);
+        // prev[0]=arr[0][0];
         
         for(int i=0;i<m;i++){
+            vector<int> temp(n,0);
             for(int j=0;j<n;j++){
                 
-                if(i==0 && j==0)continue;
+                if(i==0 && j==0) temp[j]=arr[i][j];
                 else{
                     int up=0,left=0;
                     
                     
-                     if(i>0) up = arr[i][j]+dp[i-1][j];
+                     if(i>0) up = arr[i][j]+prev[j];
                     else{
                         up=1e9;
                     }
-                    if(j>0) left= arr[i][j]+dp[i][j-1];
+                    if(j>0) left= arr[i][j]+temp[j-1];
                     else{
                         left=1e9;
                     }
-                    dp[i][j]=min(up,left);
+                    temp[j]=min(up,left);
                 }
                
             }
+            prev=temp;
         }
         
-        return dp[m-1][n-1];
+        return prev[n-1];
         
     }
 };
