64e2d08be0fbeffea6a8098feb5bf23a50d947bf
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2024-08-27 14:21:28 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (17.37%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -25,15 +25,16 @@ public:
             vector<int> temp(n,0);
             for(int j=0;j<n;j++){
                 if(i==0 && j==0){
-                    prev[j]=1;
+                    temp[j]=1;
                 }else{
                     int up=0,down=0;
                     if(i>0) up=prev[j];
-                    if(j>0) down=prev[j-1];
-                    prev[j]=up+down;
+                    if(j>0) down=temp[j-1];
+                    temp[j]=up+down;
                 }
                 
             }
+            prev=temp;
         }
         
         return prev[n-1];
