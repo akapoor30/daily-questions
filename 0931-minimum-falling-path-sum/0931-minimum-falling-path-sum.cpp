330c8e9460fe3f3b3e634400937a5eaa58cc4877
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2024-08-29 12:38:36 +0530

    Time: 12 ms (59.57%), Space: 13.8 MB (49.05%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,32 +1,59 @@
 class Solution {
 public:
-
-
-    int minFallingPathSum(vector<vector<int>>& arr) {
-        int n=arr.size();
-        int mini=INT_MAX;
-        vector<vector<int>> dp(n ,vector<int> (n));
+    
+    int f(int i,int j,vector<vector<int>> &arr,int n,vector<vector<int>> &dp){
         
-        for(int i=0;i<n;i++){
-            dp[0][i]=arr[0][i];
-
+        if(j<0 || j>=n){
+            return 1e9;
         }
-
+        
+        if(i==0){
+           return arr[0][j];
+        }
+        
+        if(dp[i][j]!=-1)return dp[i][j];
+        
+        int s=arr[i][j]+ f(i-1,j,arr,n,dp);
+        int ld = arr[i][j] + f(i-1,j-1,arr,n,dp);
+        int rd = arr[i][j] + f(i-1,j+1,arr,n,dp);
+        
+       
+        
+         int mini=min(s,ld);
+        return dp[i][j]=min(mini,rd);
+    }
+    
+    
+    int minFallingPathSum(vector<vector<int>>& arr) {
+        int n = arr.size();
+        
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        
+        for(int j=0;j<n;j++){
+            dp[0][j]=arr[0][j];
+        }
+        
         for(int i=1;i<n;i++){
             for(int j=0;j<n;j++){
-                int leftD=0;
-                int rightD=0;
-                int up=arr[i][j]+dp[i-1][j];
-                if(j-1>=0)leftD=arr[i][j]+dp[i-1][j-1];
-                else leftD=1e9;
-                if(j+1<n)rightD=arr[i][j]+dp[i-1][j+1];
-                else rightD=1e9;
-                int minim=min(up,leftD);
-                dp[i][j]=min(minim,rightD);
+                int ld,rd;
+                int s=arr[i][j]+ dp[i-1][j];
+                if(j-1>=0) ld = arr[i][j] + dp[i-1][j-1];
+                else{
+                    ld=1e9;
+                }
+                if(j<n-1) rd = arr[i][j] + dp[i-1][j+1];
+                else{
+                    rd=1e9;
+                }
+                dp[i][j]=min(s,min(ld,rd));
             }
         }
+        
+        
+        
+        int mini = INT_MAX;
         for(int i=0;i<n;i++){
-            mini=min(mini,dp[n-1][i]);
+            mini= min(mini,dp[n-1][i]);
         }
         return mini;
     }
