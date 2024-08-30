04400b5b556681448fe0af594c3f16ed64301a5a
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2024-08-31 00:16:36 +0530

    Time: 143 ms (58.83%), Space: 128.1 MB (14.67%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-
-    bool f(int ind,int tar,vector<int>& nums,vector<vector<int>> &dp){
-        if(tar==0)return 1;
-        if(ind==0) return (nums[0]==tar);
-        if(dp[ind][tar]!=-1)return dp[ind][tar];
-        bool nt=f(ind-1,tar,nums,dp);
-        bool t=0;
-        if(nums[ind]<=tar){
-            t=f(ind-1,tar-nums[ind],nums,dp);
-        }
-        return dp[ind][tar]=t|nt;
-    }
-
-    bool canPartition(vector<int>& nums) {
-        int sum=0;
-        for(int i=0;i<nums.size();i++) sum+=nums[i];
+    
+    
+    bool f(int ind,int target,vector<int>& nums,vector<vector<int>> &dp){
         
-        vector<vector<int>> dp(nums.size(),vector<int> (sum/2+1,-1));
+        if(target==0)return 1;
+        if(ind==0)return nums[0] == target;
+        
+        if(dp[ind][target]!=-1)return dp[ind][target];
+        
+        bool nt=f(ind-1,target,nums,dp);
+        bool t=0;
+        if(target>=nums[ind]){
+            t=f(ind-1,target - nums[ind],nums,dp);
+        }
+        
+        return dp[ind][target]=t|nt;
+        
+    }
+    
+    
+    bool canPartition(vector<int>& nums) {
+        
+        int sum=0;
+        
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        
+        int n=nums.size();
+        int s=sum/2;
+        
+        vector<vector<int>> dp(n,vector<int>(s+1,-1));
+        
         if(sum%2)return 0;
-        return f(nums.size()-1,sum/2,nums,dp);
+        return f(n-1,sum/2,nums,dp);
     }
 };
