f8b4ac8332c54cfdec60f11b59aa56ef73251981
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2024-08-31 11:01:14 +0530

    Time: 325 ms (20.08%), Space: 18 MB (62.05%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -31,9 +31,32 @@ public:
         int n=nums.size();
         int s=sum/2;
         
-        vector<vector<int>> dp(n,vector<int>(s+1,-1));
+        vector<vector<bool>> dp(n,vector<bool>(s+1,0));
         
         if(sum%2)return 0;
-        return f(n-1,sum/2,nums,dp);
+        
+        for(int i=0;i<n;i++){
+            dp[i][0]=1;
+        }
+        
+        if(nums[0]<=s)dp[0][nums[0]]=1;
+        
+        for(int ind=1;ind<n;ind++){
+            
+            for(int target = 1;target<=s;target++){
+                 bool nt=dp[ind-1][target];
+                bool t=0;
+                if(target>=nums[ind]){
+                    t=dp[ind-1][target - nums[ind]];
+                }
+
+                 dp[ind][target]=t|nt;
+            }
+            
+        }
+        
+        
+        
+        return dp[n-1][s];
     }
 };
