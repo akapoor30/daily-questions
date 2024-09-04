99359226ab47d60ae6ce9d80ec8a7f7a2fdeeb80
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2024-09-04 12:09:38 +0530

    Time: 8 ms (65.70%), Space: 14.1 MB (54.79%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -35,8 +35,29 @@ public:
         if(sum-target<0 || (sum-target)%2)return 0;
         int n=nums.size();
         
-        vector<vector<int>> dp(n,vector<int>(s+1,-1));
+        vector<vector<int>> dp(n,vector<int>(s+1,0));
         
-        return f(n-1,s,nums,dp);
+        
+        if(nums[0]==0)dp[0][0]=2;
+        else dp[0][0]=1;
+        
+        if(nums[0]!=0 && s>=nums[0]){
+            dp[0][nums[0]]=1;
+            
+        }
+        
+        for(int ind=1;ind<n;ind++){
+            for(int tar=0;tar<=s;tar++){
+                int nt=dp[ind-1][tar];
+                int t=0;
+                if(tar>=nums[ind]){
+                    t=dp[ind-1][tar-nums[ind]];
+                }
+
+                 dp[ind][tar]=t+nt;
+            }
+        }
+        
+        return dp[n-1][s];
     }
 };
