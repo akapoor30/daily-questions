4d4af92674c7ebc8ffc35155c641ccd7c038bd78
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2024-09-17 18:49:27 +0530

    Time: 12 ms (62.07%), Space: 16.4 MB (67.45%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -13,12 +13,22 @@ public:
     }
     
     
-    int minDistance(string word1, string word2) {
-        int n=word1.size(),m=word2.size();
+    int minDistance(string s1, string s2) {
+        int n=s1.size(),m=s2.size();
         
-        vector<vector<int>> dp(n,vector<int>(m,-1));
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
         
-        int LCS=lcs(n-1,m-1,word1,word2,dp);
+        
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                 if(s1[ind1-1]==s2[ind2-1]) dp[ind1][ind2]=1+dp[ind1-1][ind2-1];
+        
+                else dp[ind1][ind2]=max(dp[ind1][ind2-1],dp[ind1-1][ind2]);
+        
+            }            
+        }
+        
+        int LCS=dp[n][m];
         
         int deletion=n-LCS;
         int insertion=m-LCS;
