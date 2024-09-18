df874200ceda52f0601843c4b7dfaf2e42071cc1
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2024-09-18 18:20:14 +0530

    Time: 3 ms (93.20%), Space: 12.9 MB (65.62%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -14,12 +14,30 @@ public:
         }
     }
     
-    int minDistance(string word1, string word2) {
-        int n=word1.size(),m=word2.size();
+    int minDistance(string s1, string s2) {
+        int n=s1.size(),m=s2.size();
         
-        vector<vector<int>> dp(n,vector<int>(m,-1));
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
         
-        return f(n-1,m-1,word1,word2,dp);
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]) dp[i][j]=0+dp[i-1][j-1];
+        
+                else{
+                     dp[i][j]=min(1+dp[i-1][j],min(1+dp[i][j-1],1+dp[i-1][j-1]));
+                }
+            }
+        }
+        
+        
+        return dp[n][m];
         
         
     }
