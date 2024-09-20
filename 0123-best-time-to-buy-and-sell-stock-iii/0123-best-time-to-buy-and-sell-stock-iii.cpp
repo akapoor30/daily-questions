f61e32701313ec43290d635614408a498030276d
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2024-09-20 20:27:36 +0530

    Time: 431 ms (50.57%), Space: 210.3 MB (39.99%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -22,24 +22,28 @@ public:
         int n = arr.size();
         vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
         
+        vector<vector<int>> ahead(2,vector<int>(3,0));
+        vector<vector<int>> curr(2,vector<int>(3,0));
+        
         for(int ind = n-1; ind>=0;ind--){
             for(int buy = 0;buy<=1;buy++){
                 for(int cap=1;cap<=2;cap++){
                      int profit=INT_MAX;
         
                     if(buy){
-                        profit = max(-arr[ind]+dp[ind+1][0][cap], 0 + dp[ind+1][1][cap]);
+                        profit = max(-arr[ind]+ahead[0][cap], 0 + ahead[1][cap]);
                     }
                     else{
-                        profit = max(arr[ind]+dp[ind+1][1][cap-1]
-                                     , 0 + dp[ind+1][0][cap]);
+                        profit = max(arr[ind]+ahead[1][cap-1]
+                                     , 0 + ahead[0][cap]);
                     }
-                     dp[ind][buy][cap]=profit;
+                     curr[buy][cap]=profit;
                 }
             }
+            ahead=curr;
         }
         
         
-        return dp[0][1][2];
+        return ahead[1][2];
     }
 };
