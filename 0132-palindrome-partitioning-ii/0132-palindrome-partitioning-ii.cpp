af746b139a2f396f93506e98fc75af86b2ab1b15
Ansh Kapoor <91844702+akapoor30@users.noreply.github.com>
2024-09-25 21:36:46 +0530

    Time: 1791 ms (6.45%), Space: 8 MB (75.87%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -12,7 +12,7 @@ public:
     }
     
     
-    int f(int i,int n, string &s,  vector<int> &dp){
+    int f(int i,int n, string s,  vector<int> &dp){
         if(i==n)return 0;
         
         if(dp[i]!=-1)return dp[i];
@@ -31,8 +31,20 @@ public:
     int minCut(string s) {
         int n=s.size();
         
-        vector<int> dp(n,-1);
+        vector<int> dp(n+1,0);
         
-        return f(0,n,s,dp) -1;
+        for(int i=n-1;i>=0;i--){
+            int mini=2001;
+            for(int j=i;j<n;j++){
+
+                if(isPalindrome(s,i,j)){
+                    int cost=1+dp[j+1];
+                    mini=min(mini,cost);
+                }
+            }
+             dp[i]=mini;
+        }
+        
+        return dp[0]-1;
     }
 };
